2e56f50f951aa34b18c744d919dbe37a05cf4c49
Estevan <estevanmusic@gmail.com>
2018-12-17 00:55:46 -0800

    changes

--- Levels/LevelSwitcher.gd
+++ Levels/LevelSwitcher.gd
@@ -8,3 +8,24 @@ func _on_level_complete(level_name, next_level_name):
 		get_tree().change_scene_to(load("res://Levels/" + next_level_name + ".tscn"))
 	else:
 		get_tree().change_scene_to(load("res://Levels/Credits.tscn"))
+
+func _screen_cap():
+	# start screen capture
+	#get_viewport().queue_screen_capture()
+	#yield(get_tree(), "idle_frame")
+	#
+	
+	# get screen capture
+	#var capture = get_viewport().get_screen_capture()
+	# save to a file
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	var capture = get_viewport().get_texture().get_data()
+	capture.flip_y()
+	capture.save_png("res://screenshot" + var2str(OS.get_unix_time()) + ".png")
+
+func _ready():
+	set_process_input(true)
+func _input(event):
+	if event.is_action_pressed("screenshot"):
+		_screen_cap()
