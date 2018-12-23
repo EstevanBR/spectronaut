bad87a40b74ebb01b7ae59d4f4e269167b6c2d92
Estevan <estevanmusic@gmail.com>
2018-12-22 17:16:01 -0800

    now you can use U/D/L/R by themsleves!

--- Levels/LEVEL.gd
+++ Levels/LEVEL.gd
@@ -9,16 +9,16 @@ signal level_completed
 var progress = 0
 
 class CanMoveResult:
-	#func _init(l, m = false, f = null, n = null):
-	func _init(l, m, f):
+	func _init(l, f = null, n = null):
+	#func _init(l, m, f):
 		color_locations = l
-		can_move = m
+		#can_move = m
 		flat = f
-		#neighbors = n
+		neighbors = n
 	var color_locations
-	var can_move
+	#var can_move
 	var flat
-	#var neighbors
+	var neighbors
 
 onready var cam_root = $CameraRoot
 
@@ -44,13 +44,26 @@ func check_can_move(v, source_color):
 	var cells = $ColorTiles.get_filtered() #.flattened_to_z(off, v.z)
 	var flat_cells = cells.cells_flattened_to_y(off, v.y)
 	
+	var noffs = [
+		Vector3( 1, 0, 0),
+		Vector3(-1, 0, 0),
+		Vector3( 0, 0, 1),
+		Vector3( 0, 0,-1)
+	]
+	
+	var neighbors = {}
+	for noff in noffs:
+		var vnoff = v + noff
+		if flat_cells.has(vnoff):# && _can_move(source_color, flat_cells[v+noff]):
+			neighbors[noff] = flat_cells[vnoff]
+	
 	if flat_cells.has(v):
 		#print("flat_cells[" + var2str(v) + "]: " + var2str(flat_cells[v]))
 		dest_color = flat_cells[v]
 	if dest_color == Color(-1,-1,-1):
 		emit_signal("level_completed", name, _next_level_name)
 		dest_color = Color(1,1,1)
-	can_move = dest_color != null && dest_color.r >= source_color.r && dest_color.g >= source_color.g && dest_color.b >= source_color.b
+	#can_move = _can_move(source_color, dest_color)
 	
 	#return CanMoveResult.new([v], can_move)
 	var congruent_positions = cells.congruent_positions_at(v, off)
@@ -66,7 +79,10 @@ func check_can_move(v, source_color):
 #	if flat_cells.has(v + Vector3(0,0,-1)):
 #		neighbors.append(v + Vector3(0,0,-1))
 	#
-	return CanMoveResult.new(congruent_positions, can_move, flat)
+	return CanMoveResult.new(congruent_positions, flat, neighbors)
+
+func _can_move(from, to):
+	return to != null && to.r >= from.r && to.g >= from.g && to.b >= from.b
 
 func _input(event):
 	if event.is_action_pressed("print_cam_offset"):
--- characters/GZEMO/GZEMO.gd
+++ characters/GZEMO/GZEMO.gd
@@ -38,25 +38,51 @@ func jump():
 
 func _check_input():
 	var v = get_node("../ColorTiles").world_to_map(get_translation())
+	var result = level_root.check_can_move(v, _shade)
 	
 	var input_vector = Vector3()
-	if Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_up"):
-		input_vector = Vector3(-1,0,0)
-	elif Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_down"):
-		input_vector = Vector3(0,0,1)
-	elif Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_up"):
-		input_vector = Vector3(0,0,-1)
-	elif Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_down"):
-		input_vector = Vector3(1,0,0)
-	else:
+	if Input.is_action_pressed("ui_left"):# && Input.is_action_pressed("ui_up"):
+		input_vector += Vector3(-1,0,0)
+	if Input.is_action_pressed("ui_right"):# && Input.is_action_pressed("ui_down"):
+		input_vector += Vector3(1,0,0)
+	if Input.is_action_pressed("ui_up"):# && Input.is_action_pressed("ui_up"):
+		input_vector += Vector3(0,0,-1)
+	if Input.is_action_pressed("ui_down"):# && Input.is_action_pressed("ui_down"):
+		input_vector += Vector3(0,0,1)
+	if input_vector.length() == 0:
 		idle()
 		#return
-	input_vector = input_vector.rotated(Vector3(0,1,0), deg2rad(cam_root.get_rotation_index() * 90))
+	input_vector = input_vector.rotated(Vector3(0,1,0), deg2rad(( cam_root.get_rotation_index() * 90) + 45) )
 	input_vector = input_vector.round()
 	
+	#var rotated_neighbors = result.neighbors#.rotated(Vector3(0,1,0), deg2rad(cam_root.get_rotation_index() * 90))
+#	if input_vector.length() > 0:
+#		print("               v: " + var2str(v))
+#		print("    input_vector: " + var2str(input_vector))
+#		print("  v+input_vector: " + var2str(v+input_vector))
+#		print("result.neighbors: " + var2str(result.neighbors))
+#		print("\n")
+	#breakpoint
+	var cardinal_input = input_vector.rotated(Vector3(0,1,0), deg2rad(-45)).round()
+	print("\n")
+	if [Vector3(1,0,0),Vector3(-1,0,0),Vector3(0,0,1),Vector3(0,0,-1)].has(cardinal_input):
+		for n in result.neighbors:
+			var rotated_neighbor = n.rotated(Vector3(0,1,0), deg2rad(-45)).round()
+			print("rotated_neighbor: " + var2str(rotated_neighbor))
+			print("  cardinal_input: " + var2str(cardinal_input))
+			#result
+			#breakpoint
+			if (rotated_neighbor.x == cardinal_input.x && cardinal_input.x != 0) \
+			|| (rotated_neighbor.z == cardinal_input.z && cardinal_input.z != 0):
+				input_vector = rotated_neighbor.rotated(Vector3(0,1,0), deg2rad(45)).round()
+				break
+			pass
+		
+		#print("cardinal: " + var2str(cardinal.round()))
+		#breakpoint
 	var t = meshes.get_global_transform()
 	
-	if input_vector.length() != 0:
+	if input_vector.length() == 1:
 		t = t.looking_at(
 			t.origin - input_vector, \
 			Vector3(0,1,0)
@@ -64,10 +90,10 @@ func _check_input():
 	meshes.set_global_transform(t)
 	
 	#assert(input_vector.z == 0)
-	var result = level_root.check_can_move(v+input_vector, _shade)
-	if result.can_move:
+	#var result = level_root.check_can_move(v, _shade)
+	if result.neighbors.has(input_vector):
 		walk()
-		var l = result.flat
+		var l = v+input_vector
 		_color_locations = result.color_locations
 		set_translation(get_node("../ColorTiles").map_to_world(l.x, l.y, l.z))
 		Beep.stop()
